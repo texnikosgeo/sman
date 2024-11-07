b76728490f555fb427e63fb7efb2bae8435ee2e6
texnikosgeo <103029638+texnikosgeo@users.noreply.github.com>
2024-11-07 12:28:28 +0200

    Update sman12.rb

--- sman12.rb
+++ sman12.rb
@@ -31,45 +31,34 @@ puts "give bet"
 bet = gets.chomp
 bek = bet.to_i
 spins << "#{bek}"
-if  bek > 24 || bek < 0 then puts '---->
-
-    elsif teamA.include?(bek) then f_step <>
-
-    elsif teamB.include?(bek) then f_step <>
-
-    elsif teamC.include?(bek) then f_step <>
-
-    elsif teamD.include?(bek) then f_step <>
-
-    elsif teamE.include?(bek) then f_step <>
-
-    elsif teamF.include?(bek) then f_step <>
-
-    elsif teamG.include?(bek) then f_step <>
-
-    end
-if  bek > 24 || bek < 0 then puts '0--2>
-    elsif f_step[-2..] == [4, 4] || f_step[>
-    elsif f_step[-2..] == [1, 1] || f_step[>
-    elsif f_step[-2..] == [1, 2] || f_step[>
-    elsif f_step[-2..] == [1, 3] || f_step[>
-    elsif f_step[-2..] == [1, 4] || f_step[>
-    elsif f_step[-2..] == [1, 5] || f_step[>
-    elsif f_step[-2..] == [1, 6] || f_step[>
+    
+if  bek > 24 || bek < 0 then puts '-----'                 
+    elsif teamA.include?(bek) then f_step << 1 | teams << 1   
+    elsif teamB.include?(bek) then f_step << 2 | teams << 2   
+    elsif teamC.include?(bek) then f_step << 3 | teams << 3   
+    elsif teamD.include?(bek) then f_step << 4 | teams << 4   
+    elsif teamE.include?(bek) then f_step << 5 | teams << 5   
+    elsif teamF.include?(bek) then f_step << 6 | teams << 6  
+    elsif teamG.include?(bek) then f_step << 7 | teams << 7                                                                     end
+if  bek > 24 || bek < 0 then puts '0--24'
+    elsif f_step[-2..] == [4, 4] || f_step[-2..] == [4, 5]  || f_step[-2..] == [5, 4] || f_step[-2..] == [5, 6] || f_step[-2..] == [6, 5] || f_step[-2..] == [6, 7] || f_step[-2..] == [7, 6] then s_step << 1
+    elsif f_step[-2..] == [1, 1] || f_step[-2..] == [3, 5]  || f_step[-2..] == [4, 6] || f_step[-2..] == [5, 7] || f_step[-2..] == [5, 3] || f_step[-2..] == [6, 4] || f_step[-2..] == [7, 5] then s_step << 2
+    elsif f_step[-2..] == [1, 2] || f_step[-2..] == [2, 1]  || f_step[-2..] == [3, 6] || f_step[-2..] == [4, 7] || f_step[-2..] == [6, 3] || f_step[-2..] == [6, 6] || f_step[-2..] == [7, 4] then s_step << 3                                              elsif f_step[-2..] == [1, 3] || f_step[-2..] == [2, 2]  || f_step[-2..] == [2, 6] || f_step[-2..] == [3, 1] || f_step[-2..] == [3, 7] || f_step[-2..] == [6, 2] || f_step[-2..] == [7, 3] then s_step << 4                                              elsif f_step[-2..] == [1, 4] || f_step[-2..] == [2, 3]  || f_step[-2..] == [2, 7] || f_step[-2..] == [3, 2] || f_step[-2..] == [4, 1] || f_step[-2..] == [5, 5] || f_step[-2..] == [7, 2] then s_step << 5
+    elsif f_step[-2..] == [1, 5] || f_step[-2..] == [1, 7]  || f_step[-2..] == [2, 4] || f_step[-2..] == [3, 3] || f_step[-2..] == [4, 2] || f_step[-2..] == [5, 1] || f_step[-2..] == [7, 1] then s_step << 6
+    elsif f_step[-2..] == [1, 6] || f_step[-2..] == [2, 5]  || f_step[-2..] == [3, 4] || f_step[-2..] == [4, 3] || f_step[-2..] == [5, 2] || f_step[-2..] == [6, 1] || f_step[-2..] == [7, 7] then s_step << 7
 
     end
-if  bek > 24 || bek < 0 then puts '---->
-    elsif f_step[-4..-3] == [4, 4] || f_ste>
-    elsif f_step[-4..-3] == [1, 1] || f_ste>
-    elsif f_step[-4..-3] == [1, 2] || f_ste>
-    elsif f_step[-4..-3] == [1, 3] || f_ste>
-    elsif f_step[-4..-3] == [1, 4] || f_ste>
-    elsif f_step[-4..-3] == [1, 5] || f_ste>
-    elsif f_step[-4..-3] == [1, 6] || f_ste>
 
-    end
-#puts "------------------------------"
-puts "first step is :", "#{f_step}"
+if  bek > 24 || bek < 0 then puts '-----'   
+elsif f_step[-4..-3] == [4, 4] || f_step[-4..-3] == [4, 5]  || f_step[-4..-3] == [5, 4] || f_step[-4..-3] == [5, 6] || f_step[-4..-3] == [6, 5] || f_step[-4..-3] == [6, 7] || f_step[-4..-3] == [7, 6] then t_step << 1        
+elsif f_step[-4..-3] == [1, 1] || f_step[-4..-3] == [3, 5]  || f_step[-4..-3] == [4, 6] || f_step[-4..-3] == [5, 7] || f_step[-4..-3] == [5, 3] || f_step[-4..-3] == [6, 4] || f_step[-4..-3] == [7, 5] then t_step << 2     
+elsif f_step[-4..-3] == [1, 2] || f_step[-4..-3] == [2, 1]  || f_step[-4..-3] == [3, 6] || f_step[-4..-3] == [4, 7] || f_step[-4..-3] == [6, 3] || f_step[-4..-3] == [6, 6] || f_step[-4..-3] == [7, 4] then t_step << 3   
+elsif f_step[-4..-3] == [1, 3] || f_step[-4..-3] == [2, 2]  || f_step[-4..-3] == [2, 6] || f_step[-4..-3] == [3, 1] || f_step[-4..-3] == [3, 7] || f_step[-4..-3] == [6, 2] || f_step[-4..-3] == [7, 3] then t_step << 4                               
+elsif f_step[-4..-3] == [1, 4] || f_step[-4..-3] == [2, 3]  || f_step[-4..-3] == [2, 7] || f_step[-4..-3] == [3, 2] || f_step[-4..-3] == [4, 1] || f_step[-4..-3] == [5, 5] || f_step[-4..-3] == [7, 2] then t_step << 5                             
+elsif f_step[-4..-3] == [1, 5] || f_step[-4..-3] == [1, 7]  || f_step[-4..-3] == [2, 4] || f_step[-4..-3] == [3, 3] || f_step[-4..-3] == [4, 2] || f_step[-4..-3] == [5, 1] || f_step[-4..-3] == [7, 1] then t_step << 6  
+elsif f_step[-4..-3] == [1, 6] || f_step[-4..-3] == [2, 5]  || f_step[-4..-3] == [3, 4] || f_step[-4..-3] == [4, 3] || f_step[-4..-3] == [5, 2] || f_step[-4..-3] == [6, 1] || f_step[-4..-3] == [7, 7] then t_step << 7                                                                                              end
+    
+put "first step is :", "#{f_step}"
 #puts "------------------------------"
 #puts "------------------------------"
 #puts "Second step is :", "#{s_step}"
